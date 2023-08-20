a4747527339fa56dffcd67b3d1c797b1655ea89b
Tatiana Yuryeva <yuryevat@list.ru>
2023-08-20 20:11:07 +0200

    VideoList upd

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -3,7 +3,7 @@ import { nanoid } from 'nanoid'
 import Video from './Video';
 
 function VideoList(props) {
-  return props.list.map(item => <Video url={item.url} date={item.date} key={nanoid()}/>);
+  return props.list.map(item => <Video {...item} key={nanoid()}/>);
 }
 
 export default VideoList
